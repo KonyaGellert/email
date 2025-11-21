d1a94a544a2e985573038edd77fbb53fc4eb55ea
KonyaGellert <konyag@kkszki.hu>
2025-11-21 11:07:21 +0100

    menü

--- email/Program.cs
+++ email/Program.cs
@@ -10,6 +10,30 @@ namespace email
     {
         static void Main(string[] args)
         {
-        }
+            //MENÜVEZÉRLÉS
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("0 - Kilépés");
+                Console.WriteLine("1 - E-mail címek listázása");
+                Console.WriteLine("2 - Uj e-mail");
+                Console.WriteLine("3 - e-mail törlese");
+                Console.WriteLine("4 - érvényes e-mailok listázása");
+
+                string valasztas = Console.ReadLine();
+                switch (valasztas)
+                {
+                    case "0":
+                        return;
+                    case "1": Console.WriteLine("E-mail címek listázása"); break;
+                    case "2": Console.WriteLine("Uj e-mail"); break;
+                    case "3": Console.WriteLine("e-mail törlese"); break;
+                    case "4": Console.WriteLine("érvényes e-mailok listázása"); break;
+                    default: Console.Clear(); Console.WriteLine("Hibás parancs"); break;
+
+                }
+                Console.ReadKey();
+
+            }   }
     }
 }
